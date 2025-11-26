77184b0ccbdd30804f40aa8fafdd731921860ae3
leymedallada03 <leymedallada@gmail.com>
2025-11-26 14:37:35 +0800

    form.js

--- js/form.js
+++ js/form.js
@@ -5,7 +5,7 @@
 /* ================================
    DYNAMIC API URL (NO HARDCODED URL)
    ================================= */
-const API_URL = "https://script.google.com/macros/s/AKfycbwvsnkQecbpRN2IsWr-MhZoM0Jl8tRubM7t1X8hT7oUjHPPXOZ4UwPntqnrkflWMOscmA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxlnzYAxWm55cAbSRI8D0PmMlIbS8lqSLRNB8Sb3iuY8FDQgehDnfck9doEco-lc6zV/exec";
 
 /* ================================= */
 
@@ -268,6 +268,7 @@ async function submitEvacuationForm(e){
     alert("Connection error: " + err.message);
   }
 }
+
 
 
 
