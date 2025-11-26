7c1200bbe1ab8031328daad9d2a785aa29c1d81f
leymedallada03 <leymedallada@gmail.com>
2025-11-26 08:39:43 +0800

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* index.js — FIXED FOR GITHUB PAGES (NO CORS ERROR) */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxtGp2_XKsa31pbZxyKcwHUoq5q_tQlLE6ko9CNK9vlHATFyeIgfJtDzDQxTCN-Sgc7zQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbznS5fEmL0qa-UOeMf4W5KWS8RJNH2VrPlN1b4IJfi25pTGHcjgYZ2wrbsbml_bqDxFOQ/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
@@ -72,3 +72,4 @@ async function handleLogin(e) {
         msg.style.color = "red";
     }
 }
+
