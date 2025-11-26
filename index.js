2df16d28cd049cd617d2a49f0bb5df3f61195e4c
leymedallada03 <leymedallada@gmail.com>
2025-11-26 14:27:41 +0800

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* index.js — FINAL WORKING VERSION FOR GITHUB PAGES + APPS SCRIPT */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxlnzYAxWm55cAbSRI8D0PmMlIbS8lqSLRNB8Sb3iuY8FDQgehDnfck9doEco-lc6zV/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwvsnkQecbpRN2IsWr-MhZoM0Jl8tRubM7t1X8hT7oUjHPPXOZ4UwPntqnrkflWMOscmA/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
@@ -92,5 +92,6 @@ async function handleLogin(event) {
         msg.style.color = "red";
     }
 }
+
 
 
