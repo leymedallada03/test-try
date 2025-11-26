8cc92654c8ec118dd7028daa5d72c72dd4a04c6a
leymedallada03 <leymedallada@gmail.com>
2025-11-26 15:18:26 +0800

    charts.js

--- js/charts.js
+++ js/charts.js
@@ -6,7 +6,7 @@
  ******************************************************/
 
 // ===== DYNAMIC API URL (NO MORE HARD-CODED URL) =====
-const API_URL = "https://script.google.com/macros/s/AKfycbxlnzYAxWm55cAbSRI8D0PmMlIbS8lqSLRNB8Sb3iuY8FDQgehDnfck9doEco-lc6zV/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzGn1DckOjNnCRBGnAjoXRJ_VkfRv8xZnbQQ_I5FdOB1sYHJJKLknbITY6XdFZIug9CEA/exec";
 
 // =====================================================
 
@@ -278,6 +278,7 @@ async function exportAllPNGsZip() {
     alert("ZIP export failed: " + err.message);
   }
 }
+
 
 
 
