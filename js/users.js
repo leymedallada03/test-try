2ce60957c251a9cf74316c04d42fbb1ba65af37c
leymedallada03 <leymedallada@gmail.com>
2025-11-28 11:01:46 +0800

    Update users.js

--- js/users.js
+++ js/users.js
@@ -3,7 +3,7 @@
 /* =====================================
    DYNAMIC API URL (NO HARDCODED LINKS)
    ===================================== */
-const API_URL = "https://script.google.com/macros/s/AKfycbxGJsOvQK_FQFyU39DVGjx6m9z6j4pt7mgRpNWsixjDL7D2QZ9v-5u3U6GYEiHHsAgqiA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwoeKPJG-P7ytS_3mtdP0UUlHJFF5AxQPUeHw5uf2_Q3MSaPX6nwrVHUbZv-7Jwo-S8Jw/exec";
 
 /* ===================================== */
 
@@ -377,6 +377,7 @@ async function sha256Hex(message){
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   return hashArray.map(b => b.toString(16).padStart(2,"0")).join("");
 }
+
 
 
 
