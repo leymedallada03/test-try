2b3dda2c017c9fdb83d2482741d6a268528ce428
leymedallada03 <leymedallada@gmail.com>
2026-04-10 00:38:36 +0800

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -7,7 +7,7 @@
    - Assistance (combined horizontal bar)
 */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwGFL1xNeJ4E78CVpnaQKMdx_RFYDm0jVMbVJ-KnWTE2NbNrhin6bIgr5_R8sEgC4ZGeA/exec"; // your deployment
+const API_URL = "https://script.google.com/macros/s/AKfycbxdAGmjmDECdLxCsXCvnnnLktsGXLpUKrrM7t_ZZ9aEwiJ_mB0k-CVF57khbAqU2hlX2w/exec"; // your deployment
 
 let charts = {};
 
